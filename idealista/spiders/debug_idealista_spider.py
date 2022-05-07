ef7ac2e1524acb5d715fd3695e48a4495ac0eb1f
Jose Maria Peña Espartero <jpespartero@gmail.com>
2022-05-07 21:36:04 +0200

    add - included script for debugging

---
+++ idealista/spiders/debug_idealista_spider.py
@@ -0,0 +1,7 @@
+# Only for debug
+from scrapy.crawler import CrawlerProcess
+from idealista.spiders.idealista_spider import IdealistaSpider
+
+process = CrawlerProcess()
+process.crawl(IdealistaSpider)
+process.start()
